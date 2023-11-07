8226ed8128df5789ea30512bc9d1d83cac233648
mitxela <mitxela@mitxela.com>
2023-11-07 00:33:12 +0000

    Matrix test

--- ledstud.c
+++ ledstud.c
@@ -5,32 +5,108 @@ int main()
 {
 	SystemInit();
 
-	// Enable GPIOs
-	RCC->APB2PCENR |= RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
+	RCC->APB2PCENR |= RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOD | RCC_APB2Periph_GPIOC;
 
-	// GPIO D0 Push-Pull
-	GPIOD->CFGLR &= ~(0xf<<(4*0));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+	// Anodes shuffled
+	// D0 : col1
+	// A2 : col2
+	// A1 : col3
+	// D6 : col4
+	// D5 : col5
+	// D4 : col6
+	// D3 : col7
+	// D2 : col8
+	GPIOA->CFGLR = ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*1))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2));
 
-	// GPIO D4 Push-Pull
-	GPIOD->CFGLR &= ~(0xf<<(4*4));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4);
+	GPIOD->CFGLR = ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
+				 | (GPIO_CNF_IN_FLOATING << (4*1)); // don't disable SWIO
 
-	// GPIO D6 Push-Pull
-	GPIOD->CFGLR &= ~(0xf<<(4*6));
-	GPIOD->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6);
+	// GPIO C rows / cathodes, in order
+	GPIOC->CFGLR = ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*1))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*2))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*3))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*4))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*5))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*6))
+				 | ((GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*7));
 
-	// GPIO C0 Push-Pull
-	GPIOC->CFGLR &= ~(0xf<<(4*0));
-	GPIOC->CFGLR |= (GPIO_Speed_10MHz | GPIO_CNF_OUT_PP)<<(4*0);
+#define ON  1
+#define OFF 100
+#define PAT1 0x55
+#define PAT2 0xAA
 
 	while(1)
 	{
-		GPIOD->BSHR = (1<<0) | (1<<4) | (1<<6);	 // Turn on GPIOs
-		GPIOC->BSHR = (1<<0);
-		Delay_Ms( 100 );
-		GPIOD->BSHR = (1<<16) | (1<<(16+4)) | (1<<(16+6)); // Turn off GPIOs
-		GPIOC->BSHR = (1<<16);
-		Delay_Ms( 250 );
+		GPIOC->OUTDR = 0x00FF; // display off
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<0;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT1;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 0;
+		GPIOA->OUTDR = 1<<2;
+		GPIOC->OUTDR = PAT2;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 0;
+		GPIOA->OUTDR = 1<<1;
+		GPIOC->OUTDR = PAT1;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<6;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT2;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<5;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT1;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<4;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT2;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<3;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT1;
+		Delay_Ms(ON);
+
+		GPIOC->OUTDR = 0x00FF;
+		Delay_Ms( OFF );
+
+		GPIOD->OUTDR = 1<<2;
+		GPIOA->OUTDR = 0;
+		GPIOC->OUTDR = PAT2;
+		Delay_Ms(ON);
+
 	}
 }
